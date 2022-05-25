dfa55bb9454a189a545ff3985d3f5715924ef5f3
sran4 <sran4@yahoo.com>
2022-05-25 11:02:33 -0700

    tablet

--- dashboard/models.py
+++ dashboard/models.py
@@ -20,7 +20,7 @@ class Notes(models.Model):
         verbose_name_plural = "notes"
 
     def noted(self):
-        return self.notes_for_yourself[:50]
+        return self.notes_for_yourself[:100]
 
     def __str__(self):
         return self.language
