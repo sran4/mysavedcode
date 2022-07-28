125624b9e517168dfb215067f6d78f7783920a81
sran4 <sran4@yahoo.com>
2022-07-28 10:43:02 -0700

    message

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -247,8 +247,8 @@ CKEDITOR_CONFIGS = {
         ],
         'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
         # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
-        # 'height': 291,
-        # 'width': '100%',
+        'height': 291,
+        'width': '100%',
         # 'filebrowserWindowHeight': 725,
         # 'filebrowserWindowWidth': 940,
         # 'toolbarCanCollapse': True,
