60d298ee3f8ce72e1c4197dfe268f0667e1bda74
sran4 <sran4@yahoo.com>
2022-08-02 17:04:43 -0700

    First

--- studentstudyportal/urls.py
+++ studentstudyportal/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('securelogin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('images/',ImageUploadView.as_view(),name='image-upload'),
-    
+    path('images/', ImageUploadView.as_view(), name='image-upload'),
+
     path('register/', dash_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name="dashboard/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="dashboard/logout.html"), name='logout'),
