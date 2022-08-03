abe338794731b05117c03190ec5e5e692cfc8082
sran4 <sran4@yahoo.com>
2022-08-03 14:37:42 -0700

    First

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Homework, Notes, Todo
 # Register your models here.
 
-admin.site.register(Notes)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'language ', 'slug', 'created_at', 'updated_at')
+
+
+admin.site.register(Notes, NoteAdmin)
 admin.site.register(Homework)
 admin.site.register(Todo)
